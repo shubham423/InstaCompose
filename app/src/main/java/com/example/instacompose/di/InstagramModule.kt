77184b406c62bf5478fbc251d5af5100e50badfd
shubham423 <shubh991172@gmail.com>
2022-02-24 18:08:12 +0530

    di

---
+++ app/src/main/java/com/example/instacompose/di/InstagramModule.kt
@@ -0,0 +1,33 @@
+package com.example.instacompose.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object InstagramModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuthentication(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseStorage(): FirebaseStorage {
+        return FirebaseStorage.getInstance()
+    }
+}
